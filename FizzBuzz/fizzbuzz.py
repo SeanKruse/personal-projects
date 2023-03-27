2e4cae52aada92ac3bd057ad760718fea11a7108
Sean Kruse <seanfkruse@gmail.com>
2023-03-27 17:15:58 -0600

    Update fizzbuzz.py
    Added Python implementation

--- FizzBuzz/fizzbuzz.py
+++ FizzBuzz/fizzbuzz.py
@@ -1,1 +1,27 @@
+class FizzBuzz:
+    def __init__(self):
+        pass
+    
+    def fizz_buzz(self, start, end):
+        ar = []
+        
+        index = start
+        
+        while index <= end:
+            if index % 3 == 0 and index % 5 == 0:
+                ar.append("FizzBuzz")
+            elif index % 3 == 0:
+                ar.append("Fizz")
+            elif index % 5 == 0:
+                ar.append("Buzz")
+            else:
+                ar.append(str(index))
+            
+            index += 1
+        
+        return ar
 
+# Example usage
+fizz_buzz_game = FizzBuzz()
+results = fizz_buzz_game.fizz_buzz(1, 100)
+print(results)
